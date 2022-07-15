62ca5b7dd998b047efc9f8388d8b09508da98f40
Thales Alves <thalesalves758@gmail.com>
2022-07-15 19:22:04 -0300

    feat: implement put and delete texts

--- routes/textRouter.js
+++ routes/textRouter.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getText } from "../controllers/textController.js";
+import {
+  getText,
+  deleteText,
+  updateText,
+} from "../controllers/textController.js";
 import validateTokenMiddleware from "../middlewares/validateTokenMiddleware.js";
 
 const textRouter = Router();
 
 textRouter.get("/texts", validateTokenMiddleware, getText);
+textRouter.put("/texts/:textId", validateTokenMiddleware, updateText);
+textRouter.delete("/texts/:textId", validateTokenMiddleware, deleteText);
 
 export default textRouter;

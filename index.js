00b31819ba6f0c5e12d801469b4448265656dd19
Matheus Amorim <mtsgamorim@gmail.com>
2022-07-15 16:32:03 -0300

    middlewares auth working

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
 app.use(json());
 app.use(cors());
 dotenv.config();
+
+app.use(authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import {
+  validateSignIn,
+  validateSignUp,
+} from "../middlewares/validateAuthMiddlewares.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-in");
+authRouter.post("/sign-in", validateSignIn);
 
-authRouter.post("/sign-up");
+authRouter.post("/sign-up", validateSignUp);
 
 export default authRouter;

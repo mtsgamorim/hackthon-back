64812ca20c0e06af163cb73305d02758c48e8d2c
Thales Alves <thalesalves758@gmail.com>
2022-07-15 19:49:16 -0300

    feat: use regex validation

--- schemas/textSchemas.js
+++ schemas/textSchemas.js
@@ -4,5 +4,8 @@ export const textSchema = joi.object({
   title: joi.string().required(),
   email: joi.string().email().required(),
   text: joi.string().required(),
-  date: joi.string().required(),
+  date: joi
+    .string()
+    .pattern(/^[0-9]{2}\/[0-9]{2}\/[0-9]{2}$/)
+    .required(),
 });

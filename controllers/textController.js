be7fcf0b1d145373e323c8a375c89c5a485ed77c
Matheus Amorim <mtsgamorim@gmail.com>
2022-07-15 18:32:19 -0300

    get-texts testing

--- controllers/textController.js
+++ controllers/textController.js
@@ -3,15 +3,8 @@ import { ObjectId } from "mongodb";
 import db from "../db.js";
 
 export async function getText(req, res) {
-  const authorization = req.headers.authorization;
-  const token = authorization?.replace("Bearer ", "");
+  const { user } = res.locals;
   try {
-    const findUser = await db.collection("sessions").findOne({ token });
-    console.log(findUser);
-    const user = await db
-      .collection("users")
-      .findOne({ _id: new ObjectId(findUser.userId) });
-    console.log(user);
     const texts = await db
       .collection("texts")
       .find({ email: user.email })
--- routes/textRouter.js
+++ routes/textRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getText } from "../controllers/textController";
+import { getText } from "../controllers/textController.js";
+import validateTokenMiddleware from "../middlewares/validateTokenMiddleware.js";
 
 const textRouter = Router();
 
-textRouter.get("/texts", getText);
+textRouter.get("/texts", validateTokenMiddleware, getText);
 
 export default textRouter;
